e1312bf4880b255b08723b0a1aba1d7ea8181c8a
Beat Rohrer <beat@rohrer.me>
2012-06-22 17:28:03 +0200

    moved css file inclusion from typo setup to pi1 file

--- pi1/class.tx_zhdkmadekplayer_pi1.php
+++ pi1/class.tx_zhdkmadekplayer_pi1.php
@@ -119,7 +119,8 @@ class tx_zhdkmadekplayer_pi1 extends tslib_pibase {
 		//get set content
 		$this->fetchData();
 		$GLOBALS['TSFE']->additionalHeaderData['galleriffic_js'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('zhdk_madekplayer') . 'res/js/jquery.galleriffic.js"></script>';
-		
+		$GLOBALS['TSFE']->additionalHeaderData['zhdk_madekplayer_css'] = '<link rel="stylesheet" type="text/css" href="' . $this->conf['cssFile'] . '"></script>';
+
 		//get template
 		$this->templateFile = $this->cObj->fileResource($this->conf['templateFile']);
 		$subparts['template'] = $this->cObj->getSubpart($this->templateFile, '###TEMPLATE1###');
